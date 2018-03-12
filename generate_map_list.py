3025ef1a37ea70e5e0afd3b72be104ef411fe6f6
Il Jae Lee <iljae@umich.edu>
2018-03-12 11:59:15 -0400

    Updated generate script file

--- generate_map_list.py
+++ generate_map_list.py
@@ -1,7 +1,13 @@
-def write_to_file(prefix, map_nums):
+import argparse
+
+parser = argparse.ArgumentParser(description='Run the compilation')
+parser.add_argument('--test', nargs='?', const=True, help='Print to stdout')
+args = parser.parse_args()
+
+
+def write_to_file(prefix, map_nums, mode='training'):
     with open('{}_maps.txt'.format(prefix), 'w') as f:
-        f.write(','.join('training-09x09-{:04d}'.format(map_num)
-                         for map_num in map_nums))
+        f.write(','.join('{}-09x09-{:04d}'.format(mode, map_num) for map_num in map_nums))
 
 
 def generate_map_list(num, exclude_maps=frozenset()):
@@ -9,15 +15,19 @@ def generate_map_list(num, exclude_maps=frozenset()):
 
 
 def main():
-    static_maps = [127, 169, 246, 336, 445, 589, 691, 828, 844, 956]
-    exclude_maps = frozenset([2, 3, 501, 223, 543])
+    if not args.test:
+        static_maps = [127, 169, 246, 336, 445, 589, 691, 828, 844, 956]
+        exclude_maps = frozenset([2, 3, 501, 223, 543])
 
-    write_to_file('static', static_maps)
-    write_to_file('1', [1])
-    write_to_file('10', generate_map_list(10, exclude_maps))
-    write_to_file('100', generate_map_list(100, exclude_maps))
-    write_to_file('500', generate_map_list(500, exclude_maps))
-    write_to_file('1000', generate_map_list(1000, exclude_maps))
+        write_to_file('static', static_maps)
+        write_to_file('1', [1])
+        write_to_file('10', generate_map_list(10, exclude_maps))
+        write_to_file('100', generate_map_list(100, exclude_maps))
+        write_to_file('500', generate_map_list(500, exclude_maps))
+        write_to_file('1000', generate_map_list(1000, exclude_maps))
+    else:
+        write_to_file('100', generate_map_list(100))
+        write_to_file('200', generate_map_list(100))
 
 
 if __name__ == '__main__':
